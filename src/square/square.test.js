fbb0465c21bcf93cd9c5b485b2c008c80120300a
Alyubomirov1995 <Profffundo@gmail.com>
2024-05-13 18:13:08 +0300

    first commit

---
+++ src/square/square.test.js
@@ -0,0 +1,22 @@
+const square = require('./square');
+
+describe('square', () => {
+   test('Корректное значение', () => {
+   // expect(square(2)).toBe(4);
+   // expect(square(2)).toBeLessThan(5);
+   // expect(square(2)).toBeGreaterThan(3);
+   // expect(square(2)).not.toBeUndefined();
+   const spyMathPow = jest.spyOn(Math, 'pow');
+   square(2);
+   expect(spyMathPow).toBeCalledTimes(1);
+}) 
+ test('Корректное значение', () => {
+   // expect(square(2)).toBe(4);
+   // expect(square(2)).toBeLessThan(5);
+   // expect(square(2)).toBeGreaterThan(3);
+   // expect(square(2)).not.toBeUndefined();
+   const spyMathPow = jest.spyOn(Math, 'pow');
+   square(1);
+   expect(spyMathPow).toBeCalledTimes(0);
+})
+})
